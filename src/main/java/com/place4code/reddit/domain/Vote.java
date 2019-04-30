c7e31e0d02de6546eef0104901f681c628a76992
place4code <place4code@gmail.com>
2019-04-30 16:14:32 +0200

    created entities using lombok

---
+++ src/main/java/com/place4code/reddit/domain/Vote.java
@@ -0,0 +1,25 @@
+package com.place4code.reddit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private int vote;
+
+
+}
