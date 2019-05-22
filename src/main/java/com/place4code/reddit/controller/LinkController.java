3394dba00e4f0ec849142f02dcf61b49c32abdeb
place4code <place4code@gmail.com>
2019-05-22 14:01:08 +0200

    add pagination

--- src/main/java/com/place4code/reddit/controller/LinkController.java
+++ src/main/java/com/place4code/reddit/controller/LinkController.java
@@ -2,11 +2,13 @@ package com.place4code.reddit.controller;
 
 import com.place4code.reddit.model.Link;
 import com.place4code.reddit.repo.LinkRepo;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @Controller
@@ -21,8 +23,22 @@ public class LinkController {
 
     //index
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("links", linkRepo.findAll());
+    public String index(HttpServletRequest request, Model model) {
+
+        int page = 0;
+        int size = 2;
+
+
+        if (request.getParameter("page") != null && !request.getParameter("page").isEmpty()) {
+            page = Integer.parseInt(request.getParameter("page")) - 1;
+        }
+
+        if (request.getParameter("size") != null && !request.getParameter("size").isEmpty()) {
+            size = Integer.parseInt(request.getParameter("size"));
+        }
+
+
+        model.addAttribute("links", linkRepo.findAll(PageRequest.of(page, size)));
         return "index";
     }
 
