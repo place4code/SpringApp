621bdfd336716e1d5c4b57f520e0049f6d652f93
place4code <place4code@gmail.com>
2019-06-08 11:55:28 +0200

    added register view

--- src/main/java/com/place4code/reddit/controller/AuthController.java
+++ src/main/java/com/place4code/reddit/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.place4code.reddit.controller;
 
+import com.place4code.reddit.model.User;
 import com.place4code.reddit.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -24,8 +26,11 @@ public class AuthController {
     }
 
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("success", false);
         return "auth/register";
     }
+
 
 }
